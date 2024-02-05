79e7361edce5104c654f075a1e81ce251ec8ba28
ViktoriaGavrosh <viktoria.gavrosh@gmail.com>
2024-02-05 14:55:34 +0300

    fix bags

--- app/src/main/java/com/viktoriagavrosh/flightsearch/ui/RouteCard.kt
+++ app/src/main/java/com/viktoriagavrosh/flightsearch/ui/RouteCard.kt
@@ -1,6 +1,5 @@
 package com.viktoriagavrosh.flightsearch.ui
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -13,10 +12,6 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
